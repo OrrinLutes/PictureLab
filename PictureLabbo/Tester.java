57f008f7d2f636875aff668d1f369b0c8e272790
Mike Wazowski (Orrin Lutes) <onlutes@stu.naperville203.org>
2018-05-04 09:20:06 -0500

    need to write thinmg

--- PictureLabbo/Tester.java
+++ PictureLabbo/Tester.java
@@ -13,12 +13,38 @@ public class Tester
     public static void main(String[] args)
     {
         images my_guy = new images("images\\guyfieri.jpg");
+        //methods and creation of pictures
         //my_guy.explore();
         //my_guy.grayscale();
         //my_guy.write("images\\blandville.jpg");
         //my_guy.flipVerticalt();
+        //my_guy.explore();
         //my_guy.write("images\\FlavorTownGoneWrong.jpg");
-        my_guy.flipVerticalOtherWay();
-        my_guy.explore();
+        //my_guy.flipVerticalOtherWay();
+        //my_guy.colorSwap();
+        //my_guy.write("images\\Guy_Hulk.jpg");
+        //my_guy.AmericanTown();
+        //my_guy.write("images\\my_america.jpg");
+        //my_guy.infiniteGuy(2);
+        //my_guy.explore();
+        //my_guy.write("images\\FlavorFlavorFlavorFlavorFlavor.jpg");
+        
+        //open images to create collage
+        images Bland = new images("images\\blandville.jpg");
+        images Wrong = new images("images\\FlavorTownGoneWrong.jpg");
+        images Hulk = new images("images\\Guy_Hulk.jpg");
+        images America = new images("images\\my_america.jpg");
+        images Flavor = new images("images\\FlavorFlavorFlavorFlavorFlavor.jpg");
+        images Canvas = new images("images\\Guy_Canvas.png");
+        Canvas.copyGuy(0,0,my_guy);
+        Canvas.copyGuy(580,0,Bland);
+        Canvas.copyGuy(1160,0,Wrong);
+        Canvas.copyGuy(0,357,Hulk);
+        Canvas.copyGuy(580,357,Flavor);
+        Canvas.copyGuy(1160,357,America);
+        Canvas.write("images\\collage.jpg");
+        Canvas.explore();
+        
+        
     }
 }
--- PictureLabbo/images.java
+++ PictureLabbo/images.java
@@ -79,6 +79,7 @@ public class images extends SimplePicture
             pixelObj.setGreen(Avg);
         }
     }
+
     public void flipVerticalt()
     {
         int width = this.getWidth();
@@ -95,6 +96,7 @@ public class images extends SimplePicture
             }
         }
     }
+
     public void flipVerticalOtherWay()
     {
         int width = this.getWidth();
@@ -103,12 +105,134 @@ public class images extends SimplePicture
         Pixel rightPixel = null;
         for(int y=0; y < getHeight(); y++)
         {
-            for (int x= 0; x<width; x++)
+            for (int x= mirrorPoint; x<width; x++)
             {
                 rightPixel = getPixel(x,y);
-                leftPixel = getPixel(width-1-x,y);
+                leftPixel = getPixel(width-x,y);
                 leftPixel.setColor(rightPixel.getColor());
             }
         }
     }
+
+    public void colorSwap()
+    {
+        Pixel sourcePicture[];
+        sourcePicture = this.getPixels();
+        int val,Green,Red,Blue;
+        for (Pixel pixelObj : sourcePicture)
+        {
+            Green = pixelObj.getGreen();
+            Red = pixelObj.getRed();
+            Blue =  pixelObj.getBlue();
+
+            val = Green;
+            Green = Red;
+            Red = Blue;
+            Blue = val;
+
+            pixelObj.setGreen(Green);
+            pixelObj.setRed(Red);
+            pixelObj.setBlue(Blue);
+
+        }
+    }
+
+    public void AmericanTown()
+    {
+        images America2 = (new images("images\\America2.jpg"));
+        //images my_guy = (new images("images\\guyfieri.jpg"));
+
+        Pixel sourcePixel = null;
+        //int sourceColor = 0;
+        Pixel targetPixel = null;
+        //int targetColor= 0;
+        //int paintBucket = 0;
+
+        for(int sourceX = 0, targetX=0;
+        sourceX < America2.getWidth();
+        sourceX = sourceX+2, targetX = targetX+2)
+        {
+            for (int sourceY = 0, targetY=0;
+            sourceY < America2.getHeight();
+            sourceY = sourceY+2, targetY = targetY+2)
+            {
+                sourcePixel = America2.getPixel(sourceX,sourceY);
+                targetPixel= this.getPixel(targetX,targetY);
+                targetPixel.setColor(sourcePixel.getColor());
+                /*
+                //mix red
+                sourceColor = sourcePixel.getRed();
+                targetColor = targetPixel.getRed();
+                paintBucket = (sourceColor + targetColor)/2;
+                targetPixel.setRed(paintBucket);
+                //mix blue
+                sourceColor = sourcePixel.getBlue();
+                targetColor = targetPixel.getBlue();
+                paintBucket = (sourceColor + targetColor)/2;
+                targetPixel.setRed(paintBucket);
+                //mix green
+                sourceColor = sourcePixel.getGreen();
+                targetColor = targetPixel.getGreen);
+                paintBucket = (sourceColor + targetColor)/2;
+                targetPixel.setRed(paintBucket);
+                /**/
+
+            }
+        }
+    }
+
+    public void infiniteGuy(int times)
+    {
+       images pic =(new images(this));
+       Pixel sourcePixel = null;
+       Pixel targetPixel = null;
+        
+        for(int sourceX = 0, targetX=0;
+        sourceX < this.getWidth();
+        sourceX = sourceX + times, targetX++)
+        {
+            for (int sourceY = 0, targetY=0;
+            sourceY < pic.getHeight();
+            sourceY = sourceY + times, targetY++)
+            {
+                sourcePixel = pic.getPixel(sourceX,sourceY);
+                targetPixel = this.getPixel(targetX,targetY);
+                targetPixel.setColor(sourcePixel.getColor());
+            }
+        }
+        
+      if (times == 128)
+      {
+          return;
+        }
+      else
+      {
+          infiniteGuy(times*2);
+        }
+    }
+    public void copyGuy(int xOff, int yOff, images Source)
+    {
+        images sourceFile = new images(Source);
+        images sourcePicture = new images(this);
+
+        Pixel sourcePixel = null;
+        Pixel targetPixel = null;
+
+        //loop throught the colomns
+        for (int sourceX = 0, targetX=xOff;
+        sourceX < sourceFile.getWidth();
+        sourceX++, targetX++)
+        {
+            //loop through the rows
+            for(int sourceY = 0, targetY=yOff;
+            sourceY < sourceFile.getHeight();
+            sourceY++, targetY++)
+            {
+                //set the target pixel color to the spurce pixel color
+                sourcePixel = sourceFile.getPixel(sourceX,sourceY);
+                targetPixel = this.getPixel(targetX,targetY);
+                targetPixel.setColor(sourcePixel.getColor());
+            }//row loop
+        }//col loop
+    }//end of copy kait
 }
